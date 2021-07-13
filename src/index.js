b2f998789ede490823d1a4793786154a985709d9
Yenny Maldonado <yenyamaldonado@gmail.com>
2021-07-13 00:12:54 -0500

    Approved Test 7 of 8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-/*import validator from './validator.js';
+import validator from './validator.js';
 
-console.log(validator);*/
+/*const number=document.getElementById("number");
+number.addEventListener('input',replaceString());*/
+
+function getValue(){
+ const number=document.getElementById("number").value;
+ return number;
+}
+
+/*function replaceString(){
+   console.log("aquí está");
+   number.value = validator.maskify(number.value); 
+
+}*/
+
+function verResultado(){
+    if(validator.isValid(getValue())){
+        document.getElementById("respuesta").innerHTML="Tu tarjeta Aceptada"+validator.maskify(getValue());
+    }else{
+        document.getElementById("respuesta").innerHTML="Tu tarjeta ha sido rechazada, verifica los datos ingresados"+validator.maskify(getValue());
+    }
+}
+document.getElementById("valid").addEventListener('click', verResultado);
+
+/*const changeNumbers= document.getElementById("number");
+let addAll = 0; 
+changeNumbers.addEventListener('input', validator.replaceNumbers());*/
 
--- src/validator.js
+++ src/validator.js
@@ -1,81 +1,55 @@
-/*const validator = {
-  // ...
+const validator = {
+
+  
+  isValid(cardNumber){
+    let positionNumber;
+    let addAll=0;
+    let multiplyPair ;
+
+      for (let i = cardNumber.length - 1; i >= 0; i--) {
+        positionNumber = parseInt(cardNumber.charAt(i));
+  
+        if (i % 2 != 0) {
+          multiplyPair = positionNumber * 2;
+             
+          if (multiplyPair > 9) {
+            addAll += parseInt(multiplyPair / 10) + (multiplyPair % 10)
+          } else {
+            addAll += multiplyPair;
+          }
+        } else {
+        addAll += positionNumber;
+        }
+      }
+      if (addAll % 10 == 0) {
+        console.log("tarjeta Aceptada");
+        return true;
+      } else {
+        console.log("tarjeta Rechazada");
+        return false;
+      }
+    },
+
+  maskify(userNumber) {
+    //let userNumber = changeNumbers.value;
+    let lengthReplace = userNumber.length;
+
+    if (lengthReplace >= 4) {
+      let diferencePosition = lengthReplace - 4;
+      let lastNumbers = userNumber.substring(diferencePosition);
+      let addCharacter = "";
+      for (let i = 0; i < diferencePosition; i++) {
+        
+        addCharacter += "#";
+      }
+      userNumber = addCharacter + lastNumbers;
+      //console.log(userNumber);
+      return userNumber;
+    }
+  }
 };
 
-export default validator;*/
-      
-let getNumbers= document.getElementById("valid").addEventListener('click',getCardNumber);
-const changeNumbers= document.getElementById("number");
-let addAll = 0;
-
-changeNumbers.addEventListener('input', replaceNumbers);
+export default validator;
 
 
-
-/*Condiciono la entrada de datos a sólo números del 0 al 9 mediante el uso de código ASCII*/
-function onlyNumbers(event) {
-  if(event.charCode >= 48 && event.charCode <= 57){
-    return true;
-   }
-   return false;        
-}
-
-/* Declaro una variable donde pueda guardar el número que ingresa el usuario, posteriormente creo una función que primero obtiene el tamaño de la cadena alojada en mi var cardNumber */
-let cardNumber;
-function getCardNumber(){
-  cardNumber = changeNumbers.value;
-  let longNumber = cardNumber.length;
-
-/*Creo una variable que pueda ir guardando los valores obtenidos en cada ciclo, y con ayuda del for recorremos cada caracter de la cadena obtenida anteriormente*/
-  let counter= 1;
-  for (let i=longNumber-1; i>=0; i--){
-    let positionNumber = parseInt(cardNumber.charAt(i));
-
-    /*Validamos si la posición es par, si el residuo es 0 entra al bloque y multiplica el numero que esta en posición par se multiplica por 2*/
-    if(counter % 2==0){
-      let multiplyPair = positionNumber *2;
-     
-      /* si el resultado de la multiplicación del bloque de arriba es mayor a 9, se suman los digitos del resultado, primero se hace una división/ 10, al resultado de la división se le suma el residuo. y se van sumando los valores asignados a addAll */
-      if(multiplyPair > 9){
-        addAll +=  parseInt(multiplyPair /10) + (multiplyPair % 10)    
-      }else{
-        addAll += multiplyPair;
-      } 
-    }else{
-      addAll += positionNumber;
-    }
-    counter++;
-  }
-  if(addAll % 10 ==0){
-    alert("tarjeta aceptada");
-  }else{
-    alert("tarjeta rechazada");
-  } 
-}
-function replaceNumbers(){
-  let userNumber = changeNumbers.value;
-  let lengthReplace = userNumber.length;
-  
-  if(lengthReplace>=4){
-    let diferencePosition = lengthReplace-4;
-    
-    let lastNumbers = userNumber.substring(diferencePosition);
-    console.log(lastNumbers);
-    let addCharacter = "";
-      for(i=0; i<diferencePosition; i++){
-       // console.log(userNumber.charAt(i));
-       addCharacter+= "#";
-
-      }
-    //let valueReplace = addCharacter.replace(addCharacter, "#");
-    //console.log(valueReplace);
-    //console.log(addCharacter+lastNumbers);
-    changeNumbers.value = addCharacter+lastNumbers;
-  }
-
-}
-
-
- 
-/* Función para enmascarar los dígitos de la tarjeta*/
 
